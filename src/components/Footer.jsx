fb6b66227e28dceb39081a170224f4eb5ab03a61
Gourav Taneja <gouravtaneja@Gouravs-MacBook-Air.local>
2026-01-24 14:18:47 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ export default function Footer() {
             <div className="container">
                 <div className="row row-gap-5">
                     <div className="col-lg-6 col-12">
-                        <img src="/src/assets/images/footer-logo.svg" alt="logo" className="w-100 max-w-316" />
+                        <img src="/assets/images/footer-logo.svg" alt="logo" className="w-100 max-w-316" />
                         <p className="footer-description text-light fw-sm fs-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc efficitur urna in dictum suscipit. Suspendisse maximus ipsum sem.c</p>
                         <div className="d-flex justify-content-between width-100">
-                            <img src="/src/assets/images/insta.svg" alt="instagram" className="social-media" />
-                            <img src="/src/assets/images/facebook.svg" alt="facebook" className="social-media" />
-                            <img src="/src/assets/images/twitter.svg" alt="twitter" className="social-media" />
+                            <img src="/assets/images/insta.svg" alt="instagram" className="social-media" />
+                            <img src="/assets/images/facebook.svg" alt="facebook" className="social-media" />
+                            <img src="/assets/images/twitter.svg" alt="twitter" className="social-media" />
                         </div>
                     </div>
 
@@ -38,15 +38,15 @@ export default function Footer() {
                             <div className="col-lg-6 col-12">
                                 <h3 className="main fw-xl fs-md text-light">GALLERY</h3>
                                 <div className="grid-parent">
-                                    <div className="div11"> <img src="/src/assets/images/g1.png" alt="" className="w-100" />
+                                    <div className="div11"> <img src="/assets/images/g1.png" alt="" className="w-100" />
                                     </div>
-                                    <div className="div12"><img src="/src/assets/images/g2.png" alt="" className="w-100" />
+                                    <div className="div12"><img src="/assets/images/g2.png" alt="" className="w-100" />
                                     </div>
-                                    <div className="div31"><img src="/src/assets/images/g3.png" alt="" className="w-100" />
+                                    <div className="div31"><img src="/assets/images/g3.png" alt="" className="w-100" />
                                     </div>
-                                    <div className="div14"><img src="/src/assets/images/g4.png" alt="" className="w-100" />
+                                    <div className="div14"><img src="/assets/images/g4.png" alt="" className="w-100" />
                                     </div>
-                                    <div className="div15"><img src="/src/assets/images/g5.png" alt="" className="w-100" />
+                                    <div className="div15"><img src="/assets/images/g5.png" alt="" className="w-100" />
                                     </div>
                                     <div className="show-box"><a href="#" className=" text-light show-more fw-xl fs-sm">Show <br /> More</a>
                                     </div>
